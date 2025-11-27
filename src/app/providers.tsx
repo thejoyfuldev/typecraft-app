e939f60484c08f2c4d7a0f8090fb8acb610b13c8
Jason Bullard <jason@typecraft.app>
2025-11-27 09:15:49 -0600

    integrate convex provider with clerk

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,13 @@
+'use client'
+
 import { Provider as UIProvider } from '@/components/ui/provider'
 import { env } from '@/lib/env'
-import { ClerkLoaded, ClerkProvider } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/nextjs'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
 import { PropsWithChildren } from 'react'
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL!)
 
 export function Providers({ children }: PropsWithChildren) {
   return (
@@ -9,7 +15,9 @@ export function Providers({ children }: PropsWithChildren) {
       publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <ClerkLoaded>
-        <UIProvider>{children}</UIProvider>
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <UIProvider>{children}</UIProvider>
+        </ConvexProviderWithClerk>
       </ClerkLoaded>
     </ClerkProvider>
   )
