e3df99e8c60a6d147bcf5af1626d81a9f7e2af46
Jason Bullard <jason@typecraft.app>
2025-11-27 08:32:00 -0600

    optimize Chakra import and toggle compiler emotion flag

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'],
+  },
+  compiler: {
+    emotion: true,
+  },
 }
 
 export default nextConfig
