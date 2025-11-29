96cf60da0f408f4493249440b5df560e0a2b83e3
Jason Bullard <jason@typecraft.app>
2025-11-29 08:18:03 -0600

    refactor: reorganize layout structure and improve loading states in Page component

--- src/app/(app)/docs/page.tsx
+++ src/app/(app)/docs/page.tsx
@@ -4,9 +4,10 @@ import DocGrid from '@/components/DocGrid'
 import DocsEmptyState from '@/components/DocsEmptyState'
 import DocsLoading from '@/components/DocsLoading'
 import NewDocButton from '@/components/NewDocButton'
+import SearchDocsInput from '@/components/SearchDocsInput'
 import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
-import { Heading, HStack, Stack } from '@chakra-ui/react'
+import { Heading, HStack, Stack, Text } from '@chakra-ui/react'
 import { useQuery } from 'convex/react'
 
 export default function Page() {
@@ -21,11 +22,22 @@ export default function Page() {
 
   return (
     <Stack gap="8" flex="1" h="full">
-      <HStack justify="space-between" align="start">
-        <Skeleton loading={isLoading}>
+      <Stack>
+        <Skeleton loading={isLoading} w="fit">
           <Heading as="h2" textStyle="3xl">
             Docs
           </Heading>
+        </Skeleton>
+        <Skeleton loading={isLoading} w="fit">
+          <Text color="fg.muted" textStyle="sm">
+            Manage your awards, performance briefs, and decorations.
+          </Text>
+        </Skeleton>
+      </Stack>
+
+      <HStack align="center" justify="space-between" gap="4">
+        <Skeleton loading={isLoading} maxW="xl" w="full">
+          <SearchDocsInput />
         </Skeleton>
 
         <Skeleton loading={isLoading}>
